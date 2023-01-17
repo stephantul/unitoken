853ecf009662afb27b9e52b9a2d247e34004fb5f
stephantul <stephantul@gmail.com>
2023-01-17 13:10:07 +0100

    Add simple test

---
+++ tests/test_unitoken.py
@@ -0,0 +1,28 @@
+import unittest
+from unitoken import tokenize, sent_tokenize
+
+
+class TestUnitoken(unittest.TestCase):
+    def test_tokenize_empty(self) -> None:
+        empty = ""
+        with self.assertRaises(ValueError):
+            sent_tokenize(empty)
+
+        result = sent_tokenize(empty, score_threshold=0.0)
+        self.assertEqual(result, [])
+
+    def test_tokenize(self) -> None:
+        my_string = "de de de de de de"
+
+        with self.assertRaises(ValueError):
+            tokenize(my_string)
+
+        result = tokenize(my_string, score_threshold=0.0)
+        self.assertEqual(result, my_string.split())
+
+    def test_roundtrip(self) -> None:
+        my_string = "the dog walked home, and ate nice cookies down by the bay."
+
+        tokenize_result = tokenize(my_string)
+        sent_tokenize_result = sent_tokenize(my_string)
+        self.assertEqual(sum(sent_tokenize_result, []), tokenize_result)
